a11d82677db45d37e02da01df5f425eea412ad76
Seid Korac <seidkorac@Seids-MacBook-Air.local>
2023-04-21 02:48:21 +0200

    first commit

---
+++ deep-one/src/main/java/me/elderfeed/deepone/DeepOneApplication.java
@@ -0,0 +1,13 @@
+package me.elderfeed.deepone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepOneApplication.class, args);
+	}
+
+}
---
+++ deep-one/src/test/java/me/elderfeed/deepone/DeepOneApplicationTests.java
@@ -0,0 +1,13 @@
+package me.elderfeed.deepone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepOneApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// }
+
+}
